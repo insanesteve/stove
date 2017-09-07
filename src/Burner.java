e23dbd00278ec8a10db8c1f4a442d9cd96832df7
Stephen Agee <sagee@mines.edu>
2017-09-07 12:22:08 -0600

    Added neccessary classes and variables within

--- src/Burner.java
+++ src/Burner.java
@@ -1,3 +1,15 @@
 public class Burner {
-	//added comment
+	private enum Temperature {
+		BLAZING, HOT, WARM, COLD
+	}
+	public static final int TIME_DURATION = 2;
+	private Temperature myTemperature;
+	private Setting mySetting;
+	private int timer;
+	
+	public Temperature getTemperature(){
+		return myTemperature;
+	}
+	
+	
 }
