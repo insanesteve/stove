6a137ae3ca3b1079e18e66a125a7c82e0e9baaa4
Stephen Agee <sagee@mines.edu>
2017-09-08 01:00:42 -0600

    Added functionality to update the temperature of each burner based on the given stove setting and the decrementing timer

--- src/Burner.java
+++ src/Burner.java
@@ -2,22 +2,24 @@ public class Burner {
 	private enum Temperature {
 		BLAZING, HOT, WARM, COLD
 	}
-	
+
 	public static final int TIME_DURATION = 2;
 	private Temperature myTemperature;
 	private Setting mySetting;
 	private int timer;
-	
+
 	Burner (){
 		myTemperature = Temperature.COLD;
 		mySetting = Setting.OFF;
-				
+		timer = 0;		
 	}
+
 	public Temperature getTemperature(){
 		return myTemperature;
 	}
-	
+
 	public void plusButton(){
+		timer = TIME_DURATION;
 		switch (mySetting){
 		case OFF:
 			mySetting = Setting.LOW;
@@ -32,8 +34,9 @@ public class Burner {
 			break;
 		}
 	}
-	
+
 	public void minusButton(){
+		timer = TIME_DURATION;
 		switch (mySetting){
 		case LOW:
 			mySetting = Setting.OFF;
@@ -48,9 +51,60 @@ public class Burner {
 			break;
 		}
 	}
-	
-	public void updateTemperture(){
-		
-		
+
+	public void updateTemperature(){
+		if (timer > 0) timer --;
+
+		else if (timer == 0){
+			switch (myTemperature){
+			case COLD: 
+				if (mySetting == Setting.LOW){
+					myTemperature = Temperature.WARM;
+				}
+				else if (mySetting == Setting.MEDIUM || mySetting == Setting.HIGH){
+					myTemperature = Temperature.WARM;
+					timer = TIME_DURATION;
+				}
+				break;
+			case WARM: 
+				if (mySetting == Setting.MEDIUM ){
+					myTemperature = Temperature.HOT;
+				}
+				else if (mySetting == Setting.HIGH){
+					myTemperature = Temperature.HOT;
+					timer = TIME_DURATION;
+				}
+				else if (mySetting == Setting.OFF){
+					myTemperature = Temperature.COLD;
+				}
+				break;
+			case HOT:
+				if (mySetting == Setting.HIGH){
+					myTemperature = Temperature.BLAZING;
+				}
+				else if (mySetting == Setting.LOW){
+					myTemperature = Temperature.WARM;
+				}
+				else if (mySetting == Setting.OFF){
+					myTemperature = Temperature.WARM;
+					timer = TIME_DURATION;
+				}
+				break;
+			case BLAZING:
+				if (mySetting == Setting.MEDIUM){
+					myTemperature = Temperature.HOT;
+				}
+				else if (mySetting == Setting.LOW || mySetting == Setting.OFF){
+					myTemperature = Temperature.HOT;
+					timer = TIME_DURATION;
+				}
+				break;
+			default:
+				break;
+			}
+
+		}
+
 	}
+
 }
