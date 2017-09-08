13dd3753d73016f110ab96c9c3a0c6e704f96eb9
Stephen Agee <sagee@mines.edu>
2017-09-08 00:36:57 -0600

    Wrote case statemets in Plus and Minus functions

--- src/Burner.java
+++ src/Burner.java
@@ -2,14 +2,55 @@ public class Burner {
 	private enum Temperature {
 		BLAZING, HOT, WARM, COLD
 	}
+	
 	public static final int TIME_DURATION = 2;
 	private Temperature myTemperature;
 	private Setting mySetting;
 	private int timer;
 	
+	Burner (){
+		myTemperature = Temperature.COLD;
+		mySetting = Setting.OFF;
+				
+	}
 	public Temperature getTemperature(){
 		return myTemperature;
 	}
 	
+	public void plusButton(){
+		switch (mySetting){
+		case OFF:
+			mySetting = Setting.LOW;
+			break;
+		case LOW:
+			mySetting = Setting.MEDIUM;
+			break;
+		case MEDIUM:
+			mySetting = Setting.HIGH;
+			break;
+		default:
+			break;
+		}
+	}
 	
+	public void minusButton(){
+		switch (mySetting){
+		case LOW:
+			mySetting = Setting.OFF;
+			break;
+		case MEDIUM:
+			mySetting = Setting.LOW;
+			break;
+		case HIGH:
+			mySetting = Setting.MEDIUM;
+			break;
+		default:
+			break;
+		}
+	}
+	
+	public void updateTemperture(){
+		
+		
+	}
 }
